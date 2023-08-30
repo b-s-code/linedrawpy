7a4e79296b65e393100be0e0f19fab6b47720be3
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-08-30 15:57:07 +0800

    helper script

---
+++ edges_to_png.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 examples/$1.py> examples/edges_$1.txt
+python3 src/main.py examples/edges_$1.txt > examples/output/$1.ppm
+convert examples/output/$1.ppm examples/output/$1.png
+feh examples/output/$1.png
