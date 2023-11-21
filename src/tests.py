72e741be1397d0082f09326d63332fe8f8d27ffb
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-11-21 23:05:11 +0800

    signpost test cases

--- src/tests.py
+++ src/tests.py
@@ -20,7 +20,44 @@ class TestRegressions(unittest.TestCase):
     def setUpClass(cls):
         subprocess.run(["rm", "-rf", "../test_data"])
 
-    def test_triangle(self):
+    def test_triangle_pipe(self):
+        """
+        Checks linedrawpy can operate on input provided
+        via standard input.
+        """
+        # TODO
+        pass
+    
+    def test_triangle_file(self):
+        """
+        Checks linedrawpy can take a filename as an
+        argument and use the file's contents as input.
+        """
+        # TODO
+        pass
+    
+    def test_concentric_flakes(self):
+        """
+        Tests linedrawpy can produce an image from a
+        nontrivial edge set.
+        """
+        # TODO
+        pass
+
+    def test_sin_segments(self):
+        """
+        Tests linedrawpy can produce an image from a
+        nontrivial edge set.
+        """
+        # TODO
+        pass
+
+    def test_layering(self):
+        """
+        Tests linedrawpy treats ordering of input
+        edges correctly.  I.e. last edge is closest
+        to top of image.
+        """
         # TODO
         pass
 
