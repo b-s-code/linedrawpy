a3448022b1f885d3d8353d56c761c524df59daf6
b-s-code <89502375+b-s-code@users.noreply.github.com>
2023-11-29 12:10:26 +0800

    signpost intention

--- edges_to_png.sh
+++ edges_to_png.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO : consider whether there's an appealling option
+# for obsoleting this with Makefile.
+
 python3 examples/$1.py> examples/edges_$1.txt
 python3 src/main.py examples/edges_$1.txt > examples/output/$1.ppm
 convert examples/output/$1.ppm examples/output/$1.png
